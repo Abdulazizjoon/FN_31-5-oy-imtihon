8a12790b434385edb649b5784962b972e2719677
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2024-12-16 14:45:06 +0500

    .

---
+++ js/function.js
@@ -0,0 +1,73 @@
+function createCard(data) {
+  return `
+  <div class="wrapper_header">
+          <h1 class="h1">${data[0].word}</h1>
+          <audio class="newaudio" src=""></audio>
+          <button class="bottom"><img src="./img/Path 2.svg" alt="" /></button>
+        </div>
+        <p class="biot">${data[0].phonetic} </p>
+        <div class="flex">
+          <p>${data[0].meanings[0].partOfSpeech}</p>
+          <div class="tiziqq"></div>
+        </div>
+        <div class="mining">
+          <p>Meaning</p>
+          <ul class="ul">
+            
+              ${data[0].meanings[0].definitions
+                .map((value) => {
+                  return `<li>${value.definition}</li>`;
+                })
+                .join("")}
+            
+          </ul>
+          <div class="fle">
+            <h3>Synonyms</h3>
+            <h3>${data[0].meanings[0].definitions[0].synonyms}</h3>
+          </div>
+        </div>
+        <div class="verb">
+          <div class="flex">
+            <p>verb</p>
+            <div class="tiziqq"></div>
+          </div>
+          <div class="mining">
+            <ul class="ul">
+            ${data[0].meanings[1].definitions
+              .map((value) => {
+                return `<li>${value.definition}</li>`;
+              })
+              .join("")}
+              
+              <p id="keyboard">
+              “${data[0].meanings[1].definitions[0].example}”
+              </p>
+              <div class="mining_tiziq"></div>
+              <div class="source">
+                <a class="a" href="#"><p>Source</p></a>
+                <a href="${data[0].sourceUrls[0]}"
+                  ><p style="color: #2d2d2d">
+                    ${data[0].sourceUrls[0]}
+                  </p></a
+                >
+                <img src="./img/Shape (1).svg" alt="" />
+              </div>
+            </ul>
+          </div>
+        </div>
+  `;
+}
+function err() {
+  return `
+        <img class="emoji" src="./img/😕.png" alt="">
+        <h3 class="nodefinate">No Definitions Found</h3>
+        <p class="paragref">Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.</p>
+  `;
+}
+function nall() {
+  return `
+        <p class="woops">Whoops, can’t be empty…</p>
+
+  `;
+}
+export { createCard, err, nall };
---
+++ js/script.js
@@ -0,0 +1,216 @@
+import { createCard, err, nall } from "./function.js";
+let input = document.querySelector(".search");
+let btn = document.querySelector(".btn");
+let darkinput = document.querySelector("#toggle");
+let body = document.querySelector("body");
+let select = document.querySelector("#select");
+let main = document.querySelector(".main_container");
+let h = document.querySelector("h1");
+let li = document.querySelectorAll("li");
+let moon = document.querySelector(".moon");
+let wrapper_container = document.querySelector(".wrapper_container");
+
+fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/keyboard`, {
+  method: "GET",
+})
+  .then((response) => {
+    if (response.status == 200) {
+      return response.json();
+    }
+  })
+  .then((data) => {
+    console.log(data);
+    let card = createCard(data);
+    wrapper_container.innerHTML = card;
+    let bottom = document.querySelector(".bottom");
+    let newaudio = document.querySelector(".newaudio");
+    let sum;
+    let audi = data[0].phonetics;
+    audi.forEach((value) => {
+      if (value.audio !== "") {
+        console.log(value.audio);
+        sum = value.audio;
+        newaudio.setAttribute("src", sum);
+      }
+    });
+    bottom.addEventListener("click", function () {
+      newaudio.play();
+    })
+    
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+darkinput.addEventListener("click", function () {
+  if (darkinput.checked) {
+    body.classList.add("dark");
+    select.style.backgroundColor = "#050505";
+    select.style.color = "#fff";
+    input.style.backgroundColor = "#1f1f1f";
+    input.style.color = "#fff";
+    main.style.backgroundColor = "#1f1f1f";
+    btn.style.backgroundColor = "#1F1F1F";
+    localStorage.setItem("kolor", "dark");
+
+    if (h) {
+      h.style.backgroundColor = "#050505";
+      h.style.color = "#fff";
+      h.classList.add("darc");
+    }
+
+    moon.style.color = "#A445ED";
+    li.forEach((value) => {
+      value.style.color = "#fff";
+    });
+  } else {
+    body.classList.remove("dark");
+    select.style.backgroundColor = "#fff";
+    select.style.color = "#2D2D2D";
+    input.style.backgroundColor = "#F4F4F4";
+    input.style.color = "#2D2D2D";
+    main.style.backgroundColor = "#fff";
+    btn.style.backgroundColor = "#F4F4F4";
+    if (h) {
+      h.style.backgroundColor = "#fff";
+      h.style.color = "#2D2D2D";
+      h.classList.remove("darc");
+      h.classList.add("ligh");
+    }
+
+    localStorage.setItem("kolor", "light");
+
+    li.forEach((value) => {
+      value.style.color = "#2D2D2D";
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  let res = localStorage.getItem("kolor");
+  if (res == "dark") {
+    body.classList.add("dark");
+    darkinput.checked = true;
+    select.style.backgroundColor = "#050505";
+    select.style.color = "#fff";
+    input.style.backgroundColor = "#1f1f1f";
+    input.style.color = "#fff";
+    main.style.backgroundColor = "#1f1f1f";
+
+    if (h) {
+      h.style.backgroundColor = "#050505";
+      h.style.color = "#fff";
+    }
+
+    moon.style.color = "#A445ED";
+    li.forEach((value) => {
+      value.style.color = "#fff";
+    });
+  } else {
+    body.classList.remove("dark");
+    darkinput.checked = false;
+  }
+  let sum = localStorage.getItem("font");
+  if (sum == "sanserif") {
+    body.classList += " sanserif";
+    if (h) {
+      h.classList + " sanserif";
+    }
+  }
+  if (sum == "serif") {
+    body.classList += " serif";
+    if (h) {
+      h.classList += " serif";
+    }
+  }
+  if (sum == "mono") {
+    body.classList += " mono";
+    if (h) {
+      h.classList += " mono";
+    }
+  }
+});
+
+btn.addEventListener("click", function () {
+  let sum = input.value;
+  console.log(sum);
+  if (input.value.length <= 0) {
+    input.classList.add("red");
+    input.style.outlineColor = "#FF5252";
+    wrapper_container.innerHTML = nall();
+    return;
+  } else {
+    input.classList.remove("red");
+    input.style.outlineColor = "none";
+  }
+  if (sum.length < 2) {
+    alert("Eng kamida 2ta soz yozilishi kerak!");
+    return;
+  }
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${sum}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (response.status == 200) {
+        return response.json();
+      }
+      if (response.status == 404) {
+        wrapper_container.innerHTML = err();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      let card = createCard(data);
+      wrapper_container.innerHTML = card;
+
+      let bottom = document.querySelector(".bottom");
+      let newaudio = document.querySelector(".newaudio");
+      let sum
+        let audi = data[0].phonetics
+        audi.forEach((value) => {
+          if (value.audio !== "") {
+            console.log(value.audio);
+            sum = value.audio
+            newaudio.setAttribute('src',sum)
+          }
+
+        })
+      bottom.addEventListener('click',function() {
+        newaudio.play()
+      })
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+select.addEventListener("change", function () {
+  let res = select.selectedOptions[0].text;
+  body.classList.remove("sanserif", "serif", "mono");
+  h && h.classList.remove("sanserif", "serif", "mono");
+  localStorage.removeItem("sanserif", "serif", "mono");
+  if (res == "Sans Serif") {
+    body.classList += " sanserif";
+    if (h) {
+      h.classList.add(" sanserif");
+    }
+    console.log("sans serif");
+    localStorage.setItem("font", "sanserif");
+  }
+  if (res == "Serif") {
+    body.classList += " serif";
+    if (h) {
+      h.classList.add(" serif");
+    }
+    localStorage.setItem("font", "serif");
+
+    console.log("serif");
+  }
+  if (res == "Mono") {
+    body.classList += " mono";
+    console.log("Mono");
+    if (h) {
+      h.classList.add(" mono");
+    }
+    localStorage.setItem("font", "mono");
+  }
+});
